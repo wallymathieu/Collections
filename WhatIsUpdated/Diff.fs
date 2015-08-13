c3947a8935fe7677727a134c8c1e625acc0e23ad
Oskar Gewalli <gewalli@gmail.com>
2015-08-13 11:09:46 +0200

    dictionary instead of find

--- WhatIsUpdated/Diff.fs
+++ WhatIsUpdated/Diff.fs
@@ -1,22 +1,28 @@
 ﻿namespace WhatIsUpdated
 open System
 open System.Linq
+open System.Collections
+open System.Collections.Generic
+
     module Diff = 
+        let toDic map list = 
+            Enumerable.ToDictionary(list, System.Func<_,_>(map), System.Func<_,_>(id))
+        let get (hash:IDictionary<_,_>) key =
+            hash.[key]
         
         let changed map existing incoming = 
             let mapWith = List.map map
             let (setE, setI) = (existing |> mapWith |> Set.ofList, incoming |> mapWith |>  Set.ofList)
+              
+            let (hashE, hashI) = ( toDic map existing, toDic map incoming )
             let toAdd = Set.difference setI setE
             let toDel = Set.difference setE setI
             let toChange = Set.intersect setI setE
 
-            let get list key =
-                list |> List.find (fun i -> map(i) = key)
-
             {
-                ToBeAdded = toAdd   |> Set.toList |>  List.map (get incoming) ;
-                ToBeRemoved = toDel |> Set.toList |>  List.map (get existing) ; 
-                ToChange = toChange |> Set.toList |>  List.map (get incoming)
+                ToBeAdded = toAdd   |> Set.toList |>  List.map (get hashI) ;
+                ToBeRemoved = toDel |> Set.toList |>  List.map (get hashE) ; 
+                ToChange = toChange |> Set.toList |>  List.map (get hashI)
             }
 
         let Changed map existing incoming = 
