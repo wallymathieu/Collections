6f23a4a432445ee38a81925705daecabbca49465
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2017-03-24 19:19:44 +0100

    CollectionChanges fork (#4)
    * C# core implementation
    
    * Old stuff
    
    * Change to Carable
    
    * Small changes
    
    * Namespace Carable
    
    * Remove NewFolder
    
    * Adding CollectionChanges<,>

---
+++ WhatIsUpdated/CollectionChangesExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Carable
+{
+    public static class CollectionChangesExtensions
+    {
+        public static CollectionChanges<T> CollectionChanges<T>(this IEnumerable<T> existing, IEnumerable<T> updated)
+        {
+            return existing.CollectionChanges(updated, Id);
+        }
+
+        public static CollectionChanges<T> CollectionChanges<T, TKey>(this IEnumerable<T> existing, IEnumerable<T> updated, Func<T, TKey> keySelector)
+        {
+            var e = existing.ToArray();
+            var u = updated.ToArray();
+            var dicE = existing.ToDictionary(keySelector, Id);
+            var dicU = updated.ToDictionary(keySelector, Id);
+
+            var setE = new HashSet<TKey>(dicE.Keys);
+            var setU = new HashSet<TKey>(dicU.Keys);
+
+            var toAdd = setU.Except(setE).Select(key => dicU[key]);
+            var toRemove = setE.Except(setU).Select(key => dicE[key]);
+            var toChange = setE.Intersect(setU)
+                .Select(key => new CollectionItemsThatMayHaveChanged<T>(
+                                    updated: dicU[key],
+                                    existing: dicE[key]));
+
+            return new CollectionChanges<T>(toChange: toChange, toBeAdded: toAdd, toBeRemoved: toRemove);
+        }
+
+        public static CollectionChanges<TExisting, TUpdated> CollectionChanges<TExisting, TUpdated, TKey>(this IEnumerable<TExisting> existing,
+            Func<TExisting, TKey> keySelectorExisting,
+            IEnumerable<TUpdated> updated,
+            Func<TUpdated, TKey> keySelectorUpdated)
+        {
+            var e = existing.ToArray();
+            var u = updated.ToArray();
+            var dicE = existing.ToDictionary(keySelectorExisting, Id);
+            var dicU = updated.ToDictionary(keySelectorUpdated, Id);
+
+            var setE = new HashSet<TKey>(dicE.Keys);
+            var setU = new HashSet<TKey>(dicU.Keys);
+
+            var toAdd = setU.Except(setE).Select(key => dicU[key]);
+            var toRemove = setE.Except(setU).Select(key => dicE[key]);
+            var toChange = setE.Intersect(setU)
+                .Select(key => new CollectionItemsThatMayHaveChanged<TExisting, TUpdated>(
+                                    updated: dicU[key],
+                                    existing: dicE[key]));
+
+            return new CollectionChanges<TExisting, TUpdated>(toChange: toChange, toBeAdded: toAdd, toBeRemoved: toRemove);
+        }
+
+        private static T Id<T>(T val) => val;
+    }
+}
---
+++ WhatIsUpdated/CollectionChanges`2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Carable
+{
+    public class CollectionChanges<TExisting, TUpdated> : IEquatable<CollectionChanges<TExisting, TUpdated>>
+    {
+        public CollectionChanges(IEnumerable<TUpdated> toBeAdded, IEnumerable<TExisting> toBeRemoved, IEnumerable<CollectionItemsThatMayHaveChanged<TExisting, TUpdated>> toChange)
+        {
+            ToBeAdded = toBeAdded.ToArray();
+            ToBeRemoved = toBeRemoved.ToArray();
+            ToChange = toChange.ToArray();
+        }
+
+        public IReadOnlyCollection<TUpdated> ToBeAdded { get; }
+        public IReadOnlyCollection<TExisting> ToBeRemoved { get; }
+        public IReadOnlyCollection<CollectionItemsThatMayHaveChanged<TExisting, TUpdated>> ToChange { get; }
+        public override string ToString()
+        {
+            return $"ToBeAdded: {Format(ToBeAdded)}, ToBeRemoved: {Format(ToBeRemoved)}, ToChange: {Format(ToChange)}";
+        }
+
+        private string Format<T>(IEnumerable<T> collection)
+        {
+            return String.Join(",", collection.Select(ToString));
+        }
+
+        private static string ToString<T>(T t) => t.ToString();
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as CollectionChanges<TExisting, TUpdated>);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = 0;
+                result = (result * 397) ^ ToBeAdded.ToArray().GetHashCode();
+                result = (result * 397) ^ ToBeRemoved.ToArray().GetHashCode();
+                result = (result * 397) ^ ToChange.ToArray().GetHashCode();
+                return result;
+            }
+        }
+
+        public bool Equals(CollectionChanges<TExisting, TUpdated> other)
+        {
+            if (Object.ReferenceEquals(null, other)) return false;
+            return
+                ToBeAdded.SequenceEqual(other.ToBeAdded)
+                && ToBeRemoved.SequenceEqual(other.ToBeRemoved)
+                && ToChange.SequenceEqual(other.ToChange);
+        }
+    }
+}
