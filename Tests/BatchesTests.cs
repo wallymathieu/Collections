a66d60eb7a0b1858edc6b21f7e72bb568c070b4b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-06-14 20:08:20 +0200

    +BatchesOf (#9)

---
+++ Tests/BatchesTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Xunit;
+using WallyMathieu.Collections;
+using AutoFixture.Xunit2;
+
+namespace Tests
+{
+    public class BatchesTests
+    {
+        [Fact]
+        public void Test() => Assert.Equal(new[]{
+            new []{1, 2, 3},
+            new []{4, 5, 6},
+            new []{7, 8, 9},
+            new []{10}
+        }, Enumerable.Range(1, 10).BatchesOf(3));
+
+        [Theory, AutoData]
+        public void Int_range(int size) =>
+            Assert.Equal(size, Enumerable.Range(0, size * 2 - 1).BatchesOf(2).Count());
+
+        [Theory, AutoData]
+        public void Array(int size) =>
+            Assert.Equal(size, new object[size * 2].BatchesOf(2).Count());
+    }
+}
