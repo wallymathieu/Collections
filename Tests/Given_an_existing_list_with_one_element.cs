8646e1acdaac519740abf6f30c61291545c928f1
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-06-14 20:08:39 +0200

    Rename plus and minus to only in right and only in left (#8)

--- Tests/Given_an_existing_list_with_one_element.cs
+++ Tests/Given_an_existing_list_with_one_element.cs
@@ -13,22 +13,22 @@ namespace Tests
         [Fact]
         public void When_there_is_an_incoming_element() => 
             Assert.Equal(existing.ToDictionary(Id).Compare(Cons(2, existing).ToDictionary(Id)),
-                new DictionaryComparison<int, int>(Minus: new int[0],
-                                                Plus: new int[] { 2 },
+                new DictionaryComparison<int, int>(onlyInLeft: new int[0],
+                                                onlyInRight: new int[] { 2 },
                                                 Intersection: new[] { new DictionaryComparison<int, int>.ValueIntersection(1, 1) }));
 
         [Fact]
         public void When_there_an_empty_incoming_list() => 
             Assert.Equal(existing.ToDictionary(Id).Compare(new int[0].ToDictionary(Id)),
-                new DictionaryComparison<int, int>(Minus: new int[] { 1 },
-                                                Plus: new int[0],
+                new DictionaryComparison<int, int>(onlyInLeft: new int[] { 1 },
+                                                onlyInRight: new int[0],
                                                 Intersection: new DictionaryComparison<int, int>.ValueIntersection[0]));
 
         [Fact]
         public void When_incoming_is_the_same_as_existing() => 
             Assert.Equal(existing.ToDictionary(Id).Compare(existing.ToDictionary(Id)),
-                new DictionaryComparison<int, int>(Minus: new int[0],
-                                                Plus: new int[0],
+                new DictionaryComparison<int, int>(onlyInLeft: new int[0],
+                                                onlyInRight: new int[0],
                                                 Intersection: new[] { new DictionaryComparison<int, int>.ValueIntersection(1, 1) }));
 
         private IEnumerable<T> Cons<T>(T v, T[] arr)
--- WallyMathieu.Collections/DictionaryExtensions.cs
+++ WallyMathieu.Collections/DictionaryExtensions.cs
@@ -4,8 +4,20 @@ using System.Linq;
 
 namespace WallyMathieu.Collections
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class DictionaryExtensions
     {
+        /// <summary>
+        /// Compare two dictionaries with potentially different values for key differences.
+        /// </summary>
+        /// <typeparam name="TLeft"></typeparam>
+        /// <typeparam name="TRight"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns>A comparison result</returns>
         public static DictionaryComparison<TLeft, TRight> Compare<TLeft, TRight, TKey>(
             this IDictionary<TKey,TLeft> left,
             IDictionary<TKey, TRight> right)
@@ -13,13 +25,13 @@ namespace WallyMathieu.Collections
             var setLeft = new HashSet<TKey>(left.Keys);
             var setRight = new HashSet<TKey>(right.Keys);
 
-            var plus = setRight.Except(setLeft).Select(key => right[key]);
-            var minus = setLeft.Except(setRight).Select(key => left[key]);
+            var onlyInRight = setRight.Except(setLeft).Select(key => right[key]);
+            var onlyInLeft = setLeft.Except(setRight).Select(key => left[key]);
             var intersection = 
                 setLeft.Intersect(setRight)
                 .Select(key => new DictionaryComparison<TLeft, TRight>.ValueIntersection(right: right[key], left: left[key]));
 
-            return new DictionaryComparison<TLeft, TRight>(Intersection: intersection, Plus: plus, Minus: minus);
+            return new DictionaryComparison<TLeft, TRight>(Intersection: intersection, onlyInRight: onlyInRight, onlyInLeft: onlyInLeft);
         }
     }
 }
